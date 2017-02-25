2a7b25f4db3b2e367c0ef405391c9ca2af47ab09
Alex Sharabayko <alexsharabayko@gmail.com>
2017-02-25 11:37:38 +0300

    Footer

--- src/layout/footer/footer.layout.js
+++ src/layout/footer/footer.layout.js
@@ -1,7 +1,31 @@
+import './footer.layout.scss';
+
+const EMAIL = 'youngcaritasby@gmail.com';
+const SKYPE = 'youngcaritasby';
+
+const EMAIL_LINK = `mailto:${EMAIL}`;
+const SKYPE_LINK = `skype:${SKYPE}`;
+
 export class FooterLayout extends React.Component {
   render() {
     return (
-      <footer>Footer</footer>
+      <footer className="yellow footer">
+        <div className="thin-wrapper">
+          <a href={EMAIL_LINK} className="footer-link">
+            <i className="fa fa-envelope footer-icon"></i>
+            {EMAIL}
+          </a>
+          <a href={SKYPE_LINK} className="footer-link">
+            <i className="fa fa-skype footer-icon"></i>
+            {SKYPE}
+          </a>
+
+          <span className="pull-right">
+            <i className="fa fa-map-marker footer-icon"></i>
+            Минск, площадь Свободы, 1
+          </span>
+        </div>
+      </footer>
     );
   }
 }
